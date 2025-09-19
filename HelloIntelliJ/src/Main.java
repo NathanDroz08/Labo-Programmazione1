e51e39d352c445b66acca0ace4f848c1a41763ca
NathanDroz <nathan.droz2008@gmail.com>
2025-09-19 16:25:10 +0200

    Push-1

--- HelloIntelliJ/src/Main.java
+++ HelloIntelliJ/src/Main.java
@@ -2,10 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello IDE" + args[0]);
+        System.out.println("Hello IDE " + args[0]);
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
         }
     }
-}
